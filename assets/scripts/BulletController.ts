3838f6ebe9124fdf57b00d8dddbd524c0111a844
gwo <1552571230@qq.com>
2024-09-18 22:51:46 +0800

    完成子弹飞行与销毁的逻辑

--- assets/scripts/BulletController.ts
+++ assets/scripts/BulletController.ts
@@ -1,14 +1,25 @@
-import { _decorator, Component, Node } from 'cc';
-const { ccclass, property } = _decorator;
+import {_decorator, Component, Node} from 'cc';
+
+const {ccclass, property} = _decorator;
 
 @ccclass('BulletController')
 export class BulletController extends Component {
+    @property
+    speed: number = 800;
+
     start() {
 
     }
 
-    update(deltaTime: number) {
-        
+    update(dt: number) {
+        let oldPosition = this.node.getPosition();
+        // 编写子弹移动逻辑
+        this.node.setPosition(oldPosition.x, oldPosition.y + 800 * dt)
+        // 子弹飞出屏幕之后销毁掉
+        if (this.node.getPosition().y > 852) {
+            this.node.destroy();
+        }
     }
 }
-
+
+
