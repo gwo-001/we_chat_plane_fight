89fb77cc913aa7ab0c040d39b042f3b770c856fc
gwo <1552571230@qq.com>
2024-09-21 21:33:45 +0800

    敌机自动生成和玩家死亡判定

--- assets/scripts/common/EnemyProducer.ts
+++ assets/scripts/common/EnemyProducer.ts
@@ -1,14 +1,33 @@
-import { _decorator, Component, Node } from 'cc';
-const { ccclass, property } = _decorator;
+import {_decorator, Component, director, instantiate, Node, Prefab} from 'cc';
+import {HeroController} from "db://assets/scripts/HeroController";
+
+const {ccclass, property} = _decorator;
 
 @ccclass('EnemyProducer')
 export class EnemyProducer extends Component {
-    start() {
+    @property(Prefab)
+    enemyPrefab: Prefab = null;
 
+
+    start() {
+        // 如果玩家存活，那么每一秒产生一个敌机
+        this.schedule(() => {
+            let heroController = this.getComponentInChildren(HeroController);
+            if (heroController) {
+            }
+            console.log("创建敌机……")
+            let enemyIns = instantiate(this.enemyPrefab);
+            enemyIns.setParent(director.getScene().getChildByName("Canvas"))
+            const x = Math.floor(Math.random() * 421);
+            enemyIns.setPosition(x, 850);
+        }, 0.3)
     }
 
     update(deltaTime: number) {
-        
+
     }
+
+
 }
-
+
+
